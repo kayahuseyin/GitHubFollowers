0666e8c03e39a8de8de0e5ed9a05528540804778
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-01-08 13:05:34 +0300

    Refactor

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -14,28 +14,28 @@ class NetworkManager {
     private init() {}
     
     
-    func getFollowers(for username: String, page: Int, completed: @escaping([Follower]?, ErrorMessage?) -> Void) { // Ya Array of followers gelecek ya da error mesage dondurecek
+    func getFollowers(for username: String, page: Int, completed: @escaping(Result<[Follower], GFError>) -> Void) { // Ya Array of followers gelecek ya da error mesage dondurecek
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(nil, .invalidUsername)
+            completed(.failure(.invalidUsername))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
         
             if let _ = error {
-                completed(nil, .unableToComplete)
+                completed(.failure(.unableToComplete))
                 return
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { // response varsa ve kodu da 200'se her sey OK, devam
-                completed(nil, .invalidResponse)
+                completed(.failure(.invalidResponse))
                 return
             }
             
             guard let data = data else { // data varsa sikinti yok devam
-                completed(nil, .invalidData)
+                completed(.failure(.invalidData))
                 return
             }
             
@@ -43,9 +43,9 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase // converter
                 let followers = try decoder.decode([Follower].self, from: data) // decode that data into a type of an array of followers.
-                completed(followers, nil) // no error message
+                completed(.success(followers))// no error message
             } catch {
-                completed(nil, .invalidData)
+                    completed(.failure(.invalidData))
             }
         }
         
--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum ErrorMessage: String {
+enum GFError: String, Error { // Error protokolu
     case invalidUsername = "This username created an invalid request. Please try again."
     case unableToComplete = "Unable to complete your request. Please try again."
     case invalidResponse = "Invalid response from the server, Please try again."
