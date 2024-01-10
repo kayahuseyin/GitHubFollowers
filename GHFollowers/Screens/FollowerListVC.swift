8f2c573e8935c2005fafaee0136fc09896ae12b4
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-01-10 12:52:59 +0300

    Refactor

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -16,7 +16,6 @@ class FollowerListVC: UIViewController {
     var username: String!
     var followers: [Follower] = []
     
-    
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>! // it has to know about section and items
 
@@ -42,30 +41,17 @@ class FollowerListVC: UIViewController {
     
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout()) // Tum ekrani doldur
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view)) // Tum ekrani doldur
         view.addSubview(collectionView) // ilk olusturduk sonra icerisine yerlestirdik
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
-
-    // Refactor
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout { // viewController does not need to know this function. That's why it will be removed from here.
-        let width = view.bounds.width
-        let padding : CGFloat = 12 // Kenarlardan istedigim bosluk 12. Total view'a gore ayarlayacagim icin 2 ile carpacagim.
-        let minimumItemSpacing: CGFloat = 10 // Spacing 10 olacak. Total view'a gore ayarlayacagim icin 2 ile carpacagim.
-        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40) // Height'ta Labelimiz icin yer birakiyoruz.
-        
-        return flowLayout
-    }
     
     
     func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            // her self'i optional yapmak yerine:
+            guard let self = self else { return }
             
             switch result {
             case .success(let followers):
