209a3f3a61dfc0b8b3f527715ae9df348b877bfa
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-01-17 17:43:03 +0300

    add Constants
    enum for SFSymbols

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Hüseyin Kaya on 17.01.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
