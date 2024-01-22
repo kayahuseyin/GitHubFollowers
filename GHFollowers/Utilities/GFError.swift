55fb98339a3d4b4db49b99740e2cb07da73a9c78
Hüseyin Kaya <88154777+kayahuseyin@users.noreply.github.com>
2024-01-22 18:07:30 +0300

    Add error type messages

--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -12,7 +12,8 @@ enum GFError: String, Error { // Error protokolu
     case unableToComplete = "Unable to complete your request. Please try again."
     case invalidResponse = "Invalid response from the server, Please try again."
     case invalidData = "The data received from the server was invalid. Please try again."
-    
+    case unableToFavorite = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You've already favorited this user. "
 }
 
 //Enum'larda Raw Value enumdaki tum case'lere confirm eder
